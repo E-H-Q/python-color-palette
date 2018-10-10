6fe5dd663fab2cc4d38e09a7515de148c401a3c4
Ben Zacharias <benzach0421@gmail.com>
2018-10-10 00:47:52 -0600

    removed a typo

--- palette.py
+++ palette.py
@@ -6,6 +6,6 @@ x = width/2
 y = height/2
 pic = im.load()
 print ("Image dimensions:", (im.size))
-pic[x,y] = (255,0,0) #sets pixe lcolor to solid red
+pic[x,y] = (255,0,0) #sets pixe color to solid red
 print ("Pixel color at X & Y:", pic[x,y])
 im.save("Image2.png")
