cb325252f81c6d3abc50474c18274dc445f3093c
Ben Zacharias <benzach0421@gmail.com>
2018-10-10 00:40:30 -0600

    The humble beginnings

---
+++ palette.py
@@ -0,0 +1,11 @@
+from PIL import Image
+
+im = Image.open("image.jpg")
+width, height = im.size
+x = width/2
+y = height/2
+pic = im.load()
+print ("Image dimensions:", (im.size))
+pic[x,y] = (255,0,0) #sets pixe lcolor to solid red
+print ("Pixel color at X & Y:", pic[x,y])
+im.save("Image2.png")
