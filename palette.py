adfd37a01d54bc486c4ec0e1ecdc1beb44df56f5
benzach0421 <benzach0421@gmail.com>
2018-10-12 16:25:26 -0600

    Now adds all pixel colors to a list: "xy_list". The entries of xy_list are displayed.

--- palette.py
+++ palette.py
@@ -2,10 +2,22 @@ from PIL import Image
 
 im = Image.open("image.jpg")
 width, height = im.size
-x = width/2
-y = height/2
+x = 0
+y = 0
+xy_list = []
+overflow = []
 pic = im.load()
+while (x < width - 1):
+	x += 1
+	[xy_list.append(pic[x,y]) for v in overflow if v not in xy_list]
+while (y < height - 1):
+	y += 1
+	xy_list.append(pic[x,y])
+for contents in xy_list:
+	print (contents)
+print ("Number of entries in list:",(len(xy_list)))
+print ("Coords stopped at:",x,",",y)
 print ("Image dimensions:", (im.size))
-pic[x,y] = (255,0,0) #sets pixel color to solid red
-print ("Pixel color at X & Y:", pic[x,y])
-im.save("Image2.png")
+#print ("Pixel color at X & Y:", pic[x,y])
+#pic[x,y] = (255,0,0)
+#im.save("image2.png")
