72c3e29ff21df69ac62a480c4d2effa0f3991312
Ben Zacharias <benzach0421@gmail.com>
2018-10-14 00:27:35 -0600

    RGB values are individually added to separate lists. The entries in those lists are displayed. (comments are left in for if I mess something up).

--- palette.py
+++ palette.py
@@ -4,18 +4,29 @@ im = Image.open("image.jpg")
 width, height = im.size
 x = 0
 y = 0
-xy_list = []
+R = []
+G = []
+B = []
 overflow = []
 pic = im.load()
 while (x < width - 1):
 	x += 1
-	[xy_list.append(pic[x,y]) for v in overflow if v not in xy_list]
+	r, g, b = im.getpixel((x, y))
+	R.append(r)
+	G.append(g)
+	B.append(b)
+	#[xy_list.append(pic[x,y]) for v in overflow if v not in xy_list]
 while (y < height - 1):
 	y += 1
-	xy_list.append(pic[x,y])
-for contents in xy_list:
-	print (contents)
-print ("Number of entries in list:",(len(xy_list)))
+	r, g, b = im.getpixel((x, y))
+	R.append(r)
+	G.append(g)
+	B.append(b)
+	#xy_list.append(pic[x,y])
+for contents in R,G,B:
+	print ((contents),"\n")
+length = len(R+G+B)
+print ("Number of entries in list(s):",(length))
 print ("Coords stopped at:",x,",",y)
 print ("Image dimensions:", (im.size))
 #print ("Pixel color at X & Y:", pic[x,y])
