35c1631816ea25c3561a93315b27b8cba1f6e8ab
Ben Zacharias <benzach0421@gmail.com>
2018-10-14 00:31:20 -0600

    Code janitor duties. Cleaning and spacing, making look good.

--- palette.py
+++ palette.py
@@ -4,31 +4,38 @@ im = Image.open("image.jpg")
 width, height = im.size
 x = 0
 y = 0
+pic = im.load()
+
 R = []
 G = []
 B = []
-overflow = []
-pic = im.load()
+
 while (x < width - 1):
 	x += 1
 	r, g, b = im.getpixel((x, y))
 	R.append(r)
 	G.append(g)
 	B.append(b)
-	#[xy_list.append(pic[x,y]) for v in overflow if v not in xy_list]
+
 while (y < height - 1):
 	y += 1
 	r, g, b = im.getpixel((x, y))
 	R.append(r)
 	G.append(g)
 	B.append(b)
-	#xy_list.append(pic[x,y])
+
 for contents in R,G,B:
 	print ((contents),"\n")
+
 length = len(R+G+B)
+
 print ("Number of entries in list(s):",(length))
 print ("Coords stopped at:",x,",",y)
 print ("Image dimensions:", (im.size))
+
+#EXTRAS
 #print ("Pixel color at X & Y:", pic[x,y])
 #pic[x,y] = (255,0,0)
 #im.save("image2.png")
+#[xy_list.append(pic[x,y]) for v in overflow if v not in xy_list]
+#xy_list.append(pic[x,y])
