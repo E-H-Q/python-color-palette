44fc8c87f8def7098136f3ec36026c44ea686b55
Ben Zacharias <benzach0421@gmail.com>
2018-10-27 00:23:04 -0600

    Cleaned up code for displaying the main colors

--- palette.py
+++ palette.py
@@ -29,16 +29,16 @@ for contents in R,G,B:
         print ((contents),"\n")
 
 length = len(R+G+B)
-countR = Counter(R)
-countG = Counter(G)
-countB = Counter(B)
+countR = Counter(R).most_common(5)
+countG = Counter(G).most_common(5)
+countB = Counter(B).most_common(5)
 
 print ("Number of entries in list(s):",(length))
 print ("Image dimensions:", (im.size))
 print ("Most prominent colors in image:")
-print ("        R:", countR.most_common(5))
-print ("        G:", countG.most_common(5))
-print ("        B:", countB.most_common(5),"\n")
+print ("        R:", countR)
+print ("        G:", countG)
+print ("        B:", countB,"\n")
 
-#img = Image.new("RGB", (width, height), color = ((int(sum(R) / float(len(R)))),(int(sum(G) / float(len(G)))),(int(sum(B) / float(len(B))))))
+#img = Image.new("RGB", (width, height), color = (R,G,B)
 #img.save("image2.png")
