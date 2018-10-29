13edc9fbe5fca63352d6ac6d37f411cb4265f162
Ben Zacharias <benzach0421@gmail.com>
2018-10-28 23:56:53 -0600

    All 5 prominent colors are put into "palette.png"

--- palette.py
+++ palette.py
@@ -40,7 +40,13 @@ print ("        R:", countR)
 print ("        G:", countG)
 print ("        B:", countB,"\n")
 
+x = width/5
+
 img = Image.new("RGB", (width, height))
 rect = ImageDraw.Draw(img)
-rect.rectangle(((0, 0), (width/5, height)), fill = (countR[0][0],countG[0][0],countB[0][0]))
-img.save("image2.png")
+rect.rectangle(((0, 0), (x, height)), fill = (countR[0][0],countG[0][0],countB[0][0]))
+rect.rectangle(((x, 0), (x*2, height)), fill = (countR[1][0],countG[1][0],countB[1][0]))
+rect.rectangle(((x*2, 0), (x*3, height)), fill = (countR[2][0],countG[2][0],countB[2][0]))
+rect.rectangle(((x*3, 0), (x*4, height)), fill = (countR[3][0],countG[3][0],countB[3][0]))
+rect.rectangle(((x*4, 0), (x*5, height)), fill = (countR[4][0],countG[4][0],countB[4][0]))
+img.save("palette.png")
